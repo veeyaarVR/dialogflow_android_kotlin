b07347b80556794b97d028317c62245374acebb4
vignesh <vignesh.m@cumulations.com>
2021-12-05 09:01:01 +0530

    update: code

--- app/src/main/java/com/vignesh/chatbotkotlin/MainActivity.kt
+++ app/src/main/java/com/vignesh/chatbotkotlin/MainActivity.kt
@@ -30,10 +30,6 @@ import java.util.UUID
 class MainActivity : AppCompatActivity() {
   private var messageList: ArrayList<Message> = ArrayList()
 
-  //dialogFlow
-  private var sessionsClient: SessionsClient? = null
-  private var sessionName: SessionName? = null
-  private val uuid = UUID.randomUUID().toString()
   private val TAG = "mainactivity"
   private lateinit var chatAdapter: ChatAdapter
 
@@ -57,72 +53,20 @@ class MainActivity : AppCompatActivity() {
       }
     }
 
-    //initialize bot config
     setUpBot()
   }
 
   @SuppressLint("NotifyDataSetChanged")
   private fun addMessageToList(message: String, isReceived: Boolean) {
-    messageList.add(Message(message, isReceived))
-    editMessage.setText("")
-    chatAdapter.notifyDataSetChanged()
-    chatView.layoutManager?.scrollToPosition(messageList.size - 1)
+    //handles UI changes
   }
 
   private fun setUpBot() {
-    try {
-      val stream = this.resources.openRawResource(R.raw.credential)
-      val credentials: GoogleCredentials = GoogleCredentials.fromStream(stream)
-        .createScoped("https://www.googleapis.com/auth/cloud-platform")
-      val projectId: String = (credentials as ServiceAccountCredentials).projectId
-      val settingsBuilder: SessionsSettings.Builder = SessionsSettings.newBuilder()
-      val sessionsSettings: SessionsSettings = settingsBuilder.setCredentialsProvider(
-        FixedCredentialsProvider.create(credentials)
-      ).build()
-      sessionsClient = SessionsClient.create(sessionsSettings)
-      sessionName = SessionName.of(projectId, uuid)
-      Log.d(TAG, "projectId : $projectId")
-    } catch (e: Exception) {
-      Log.d(TAG, "setUpBot: " + e.message)
-    }
+    //initiates the dialogflow
   }
 
   private fun sendMessageToBot(message: String) {
-    val input = QueryInput.newBuilder()
-      .setText(TextInput.newBuilder().setText(message).setLanguageCode("en-US")).build()
-    GlobalScope.launch {
-      sendMessageInBg(input)
-    }
+    //sends message to the bot
   }
 
-  private suspend fun sendMessageInBg(
-    queryInput: QueryInput
-  ) {
-    withContext(Default) {
-      try {
-        val detectIntentRequest = DetectIntentRequest.newBuilder()
-          .setSession(sessionName.toString())
-          .setQueryInput(queryInput)
-          .build()
-        val result = sessionsClient?.detectIntent(detectIntentRequest)
-        if (result != null) {
-          runOnUiThread {
-            updateUI(result)
-          }
-        }
-      } catch (e: java.lang.Exception) {
-        Log.d(TAG, "doInBackground: " + e.message)
-        e.printStackTrace()
-      }
-    }
-  }
-
-  private fun updateUI(response: DetectIntentResponse) {
-    val botReply: String = response.queryResult.fulfillmentText
-    if (botReply.isNotEmpty()) {
-      addMessageToList(botReply, true)
-    } else {
-      Toast.makeText(this, "something went wrong", Toast.LENGTH_SHORT).show()
-    }
-  }
 }
